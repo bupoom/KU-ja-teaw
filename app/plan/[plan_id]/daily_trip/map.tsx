d8ea48f3ea3def892995e5bc834cc90b2811dbf5
NapatKU <napat.phann@ku.th>
2025-09-24 11:17:38 +0700

    commit before prebuildd map

--- app/plan/[plan_id]/daily_trip/map.tsx
+++ app/plan/[plan_id]/daily_trip/map.tsx
@@ -9,10 +9,15 @@ import {
 import Header from "@/components/common/Header";
 import { useRouter } from "expo-router";
 import { useEffect } from "react";
+import { GoogleMaps } from "expo-maps";
+import Constants from "expo-constants";
 
 const map = () => {
   useEffect(() => {
     console.log(`I am in map.tsx`);
+    console.log(Platform.OS);
+    console.log('Google Maps API Key:', Constants.expoConfig?.android?.config?.googleMaps?.apiKey);
+    console.log('Full Expo Config:', JSON.stringify(Constants.expoConfig, null, 2));
   }, []);
   const router = useRouter();
 
@@ -24,6 +29,11 @@ const map = () => {
     <SafeAreaView className="flex-1 bg-white">
       <StatusBar barStyle="light-content" backgroundColor="#ffffff" />
       <Header title="Map" onBackPress={handleBack} />
+      {Platform.OS === "android" ? (
+        <GoogleMaps.View style={{ flex: 1 }} />
+      ) : (
+        <Text>Maps are only available on Android and iOS</Text>
+      )}
     </SafeAreaView>
   );
 };
